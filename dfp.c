04131dac40b4726238c92c719845d0271de45471
ApprentiUnity <50675988+AprentiUnity@users.noreply.github.com>
2022-12-23 17:32:49 +0100

    Ajout du code du programme dfp

---
+++ dfp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, unsigned char *argv[])
+{
+    register unsigned int i = 1;
+    for(i = 1; i < argc; i++)
+    {
+        register unsigned long long nombreDecoupe = strtoull(argv[i], NULL, 10);
+        printf("Décomposition de %lld en produits de nombres premiers\n", nombreDecoupe);
+        while(nombreDecoupe != 1)
+        {
+            if(nombreDecoupe % 2 == 0)
+            {
+                nombreDecoupe = nombreDecoupe / 2;
+                printf("2\n");
+            }
+            else if(nombreDecoupe % 3 == 0)
+            {
+                nombreDecoupe = nombreDecoupe / 3;
+                printf("3\n");
+            }
+            else if(nombreDecoupe % 5 == 0)
+            {
+                nombreDecoupe = nombreDecoupe / 5;
+                printf("5\n");
+            }
+            else if(nombreDecoupe % 7 == 0)
+            {
+                nombreDecoupe = nombreDecoupe / 7;
+                printf("7\n");
+            }
+            else
+            {
+                register unsigned long long nombrePremier = 11;
+                while(nombrePremier <= nombreDecoupe)
+                {
+                    if(nombreDecoupe % nombrePremier != 0)
+                    {
+                        nombrePremier++;
+                    }
+                    else
+                    {
+                        nombreDecoupe = nombreDecoupe / nombrePremier;
+                         printf("%lld\n", nombrePremier);
+                     }
+                 }
+            }
+        }
+    }
+    printf("Progamme éxécuté avec succès ! [v1.0]\n");
+    return 0;
+}
